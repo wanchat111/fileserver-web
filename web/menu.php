0b5c024b4d0260b63e7758ecb85745e0ec580ae7
Wanchat Damdoung <wanchat111@hotmail.com>
2017-01-25 09:39:51 +0700

    Fix link

--- web/menu.php
+++ web/menu.php
@@ -15,10 +15,10 @@
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="index.php" class="">Home</a></li>
                         <li class="active"><a href="accountList.php" class="">Accounts</a></li>
-                        <li class="active"><a href="upload.php" class="">Upload</a></li>
+                        <li class="active"><a href="index.php" class="">Upload</a></li>
                     </ul>
                     <ul class="nav navbar-nav pull-right">
-                        <li class="active"><a href="login.php" class="">User</a></li>
+                        <li class="active"><a class="">User</a></li>
                         <li class=""><a href="#">Logout</a></li>
                     </ul>
                 </div>
